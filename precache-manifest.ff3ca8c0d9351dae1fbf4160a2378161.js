cdbfb6c395b0ec97de82aef5a5734b8cf9788d23
nbouteme <nbouteme@users.noreply.github.com>
2019-01-13 22:07:06 +0100

    Add blog engine

---
+++ precache-manifest.ff3ca8c0d9351dae1fbf4160a2378161.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "9ab79c20a97f4af2392f",
+    "url": "/css/app.aeb50221.css"
+  },
+  {
+    "revision": "9ab79c20a97f4af2392f",
+    "url": "/app.bundle.js"
+  },
+  {
+    "revision": "a742227545e6476faf05",
+    "url": "/js/chunk-vendors.20665e48.js"
+  },
+  {
+    "revision": "c82aa09edcd42b07a2b50fcb65b4eb41",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b497f133772887d35240a1a8f21326cb",
+    "url": "/a.svg"
+  },
+  {
+    "revision": "19f1922b5f7980b93cdd52334daa0d3d",
+    "url": "/img/bg.png"
+  },
+  {
+    "revision": "16735f17d996f4a288f258a11e88dd08",
+    "url": "/cmunrm.otf"
+  }
+];
